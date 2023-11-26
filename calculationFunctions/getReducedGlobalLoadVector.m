476ebd5e5867250e1a7a47f15aea8c668d1c862f
User <salih.sahbegovic1998@gmail.com>
2023-11-26 15:29:02 +0100

    fixing

--- calculationFunctions/getReducedGlobalLoadVector.m
+++ calculationFunctions/getReducedGlobalLoadVector.m
@@ -5,7 +5,7 @@ for i=4:6
 reducedGlobalLoadVector(i,1) = 0;
 end
 reducedGlobalLoadVector(1,1) = reducedGlobalLoadVector(1,1) + force*cosd(angle);
-reducedGlobalLoadVector(2,1) = reducedGlobalLoadVector(2,1) - force*sind(angle);
+reducedGlobalLoadVector(3,1) = reducedGlobalLoadVector(3,1) - force*sind(angle);
 
 end
 
